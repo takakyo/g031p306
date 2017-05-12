1b421fb88a4dfef3fafc15f05784542e42e27a45
takakyo <g031p306@s.iwate-pu.ac.jp>
2017-05-12 18:35:46 +0900

    Update db_conect.php

--- homework4/db_conect.php
+++ homework4/db_conect.php
@@ -1,14 +1,17 @@
 ﻿<?php
-$db_user = 'root';     // ユーザー名
-$db_pass = 'v7hrche8'; // パスワード
-$db_name = 'bbs';     // データベース名
-// MySQLに接続
+$db_user = 'root';
+$db_pass = 'v7hrche8';
+$db_name = 'bbs';
+
 $mysqli = new mysqli('localhost', $db_user, $db_pass, $db_name);
 $result = $mysqli->query('select * from `messages`');
 $result_message = '';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   if (!empty($_POST['message'])) {
-    $mysqli->query("insert into `messages` (`body`,`name`) values ('{$_POST['message']}','{$_POST['message2']}')");
+    $mysqli->query("insert into `messages` (`body`,`name`)
+     values ('{$_POST['message']}','{$_POST['message2']}')");
+
     $result_message = 'データベースに登録しました！XD';
   } else {
     $result_message = 'メッセージを入力してください...XO';
@@ -16,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 $result = $mysqli->query('select * from `messages` order by `id` desc');
 ?>
+
 <html>
   <head>
     <meta charset="UTF-8">
@@ -24,11 +28,21 @@ $result = $mysqli->query('select * from `messages` order by `id` desc');
     <p> <?php echo $result_message; ?> </p>
     <form action="" method="post">
       題目　<input type="text" name="message" />　
-      登録者名<input type="text" name="message2" />
+      投稿者名　<input type="text" name="message2" />
       <input type="submit" />
     </form>
+    <table border="2">
+        <tr>
     <?php foreach ($result as $row) : ?>
-      <p> <?php echo $row['id']." ".$row['body']." ".$row['timestamp']." ".$row['name'];?>  </p>
+      <tr>
+        <?php
+        echo '<td>', $row['id'],'</td><td>',
+        $row['body'],'</td><td>',
+        $row['timestamp'],'</td> <td>',
+        $row['name'],'</td>';
+        ?>
+      </tr>
     <?php endforeach ; ?>
+        </table>
   </body>
 </html>
